4cadf7f943cf3a1a02d0cb9b111162b166ce6717
Rob Dearling <rob@nulldev.uk>
2024-08-16 22:44:02 +0100

    Subtests within a base test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Rob")
-	want := "Hello, Rob"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Rob")
+		want := "Hello, Rob"
 
-	if got != want {
-		t.Errorf("Got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("Got %q want %q", got, want)
+		}
+	})
 }
