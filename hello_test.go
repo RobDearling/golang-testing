62fdd5d4ce346f4fed9c8bb20d2eabb34f706f07
Rob Dearling <robbie-jay.dearling@lexisnexisrisk.com>
2024-08-19 10:45:42 +0100

    Refactor testing suite

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,20 @@ func TestHello(t *testing.T) {
 		got := Hello("Rob")
 		want := "Hello, Rob"
 
-		if got != want {
-			t.Errorf("Got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("Got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Got %q want %q", got, want)
+	}
 }
